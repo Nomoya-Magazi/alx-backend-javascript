ab217cf6c62c07e259436730d3277714c8a16956
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-03-23 16:06:42 +0000

    classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+	constructor(code, name) {
+		this._code = code;
+		this._name = name;
+	}
+
+	get code() {
+		return this.code;
+	}
+
+	set code(c) {
+		this._code = c;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(n) {
+		this._name = n;
+	}
+
+	displayFullCurrency() {
+		return `${this._name} (${this._code})`;
+	}
+}
+
