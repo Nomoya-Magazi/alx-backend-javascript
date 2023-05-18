07cfb4e24511df09073daaa42f3ab927ab48212a
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-05-18 12:19:31 +0000

    unitests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,24 +2,30 @@ const assert = require("assert");
 const { it, describe } = require("mocha");
 const calculateNumber = require("./0-calcul");
 
-describe('calculateNumber', function() {
-  it('should return the sum of two rounded numbers', function() {
-    const result = calculateNumber(1, 3);
-    assert.strictEqual(result, 4);
-  });
+describe("calculateNumber()", function() {
 
-  it('should round the numbers and return the sum', function() {
-    const result = calculateNumber(1, 3.7);
-    assert.strictEqual(result, 5);
-  });
-
-  it('should handle decimal inputs and return the sum of rounded numbers', function() {
-    const result = calculateNumber(1.2, 3.7);
-    assert.strictEqual(result, 5);
-  });
-
-  it('should handle decimal inputs and return the sum of rounded numbers', function() {
-    const result = calculateNumber(1.5, 3.7);
-    assert.strictEqual(result, 6);
-  });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.4, 2.2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.6, 2.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(0, 0);
+      assert.strictEqual(res, 0);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.6, -1.7);
+      assert.strictEqual(res, -4);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(-1.4, -1.3);
+	assert.strictEqual(res, -2);
+    });
 });
